28ccda5245e87b0dc3213caaac77ecc939c181b1
woodlinepro <woodlineprodev@gmail.com>
2024-12-05 17:51:17 +0500

    ok

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { appConfig } from './configs'
+import { PassUserIdInterceptor } from './interceptors'
 
 setImmediate(async (): Promise<void> => {
 	const app = await NestFactory.create<INestApplication>(AppModule, { cors: true })
 
 	app.use(json({ limit: '50mb' }))
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+	app.useGlobalInterceptors(new PassUserIdInterceptor())
 
 	const swaggerConfig = new DocumentBuilder().addBearerAuth().build()
 	const document = SwaggerModule.createDocument(app, swaggerConfig)
