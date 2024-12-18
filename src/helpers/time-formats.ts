8030fb2d8b64e0861273d897d9dc8085490a9604
woodlinepro <woodlineprodev@gmail.com>
2024-12-19 01:25:29 +0500

    ok

--- src/helpers/time-formats.ts
+++ src/helpers/time-formats.ts
@@ -3,14 +3,14 @@ import { toZonedTime } from 'date-fns-tz'
 
 const timeZone = 'UTC'
 
-export const getStartDate = (date: string) => {
+export const getStartDate = (date: Date) => {
 	console.log(date)
 	const zonedTime = toZonedTime(date, timeZone)
 	console.log(zonedTime, format(startOfDay(zonedTime), "yyyy-MM-dd'T'HH:mm:ss.SSSX"))
 	return startOfDay(zonedTime)
 }
 
-export const getEndDate = (date: string) => {
+export const getEndDate = (date: Date) => {
 	const zonedTime = toZonedTime(date, timeZone)
 	return endOfDay(zonedTime)
 }
