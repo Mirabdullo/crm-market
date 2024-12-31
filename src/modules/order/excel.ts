2200a9470e045c4f869c167dabceecea0a10c235
woodlinepro <woodlineprodev@gmail.com>
2024-12-31 11:26:55 +0500

    ok

--- src/modules/order/excel.ts
+++ src/modules/order/excel.ts
@@ -7,23 +7,26 @@ export async function OrderUpload(data: OrderRetriveAllResponse['data'], res: Re
 	const workbook = new ExcelJS.Workbook()
 	const worksheet = workbook.addWorksheet('продажа')
 
+	const sum = data.reduce((sum, item) => sum + item.sum, 0)
+
 	worksheet.mergeCells('A1:C1')
-	worksheet.getCell('A1').value = `Общая сумма:   ${100}`
+	worksheet.getCell('A1').value = `Общая сумма:   ${sum}`
 	worksheet.getCell('A1').font = { bold: true }
 	worksheet.addRow([])
 
-	const headerRow = worksheet.addRow(['№', 'Клиент', 'Cумма', 'Продавец', 'Информация', 'Долг', 'Дата продажа'])
+	const headerRow = worksheet.addRow(['№', 'Клиент', 'тел', 'Cумма', 'Продавец', 'Информация', 'Долг', 'Дата продажа'])
 	headerRow.font = { bold: true }
 	headerRow.alignment = { vertical: 'middle', horizontal: 'center' }
 	headerRow.height = 24
 
 	worksheet.getColumn(1).width = 5
 	worksheet.getColumn(2).width = 20
+	worksheet.getColumn(3).width = 16
 	worksheet.getColumn(3).width = 14
-	worksheet.getColumn(4).width = 16
-	worksheet.getColumn(5).width = 12
-	worksheet.getColumn(6).width = 10
-	worksheet.getColumn(6).width = 16
+	worksheet.getColumn(4).width = 18
+	worksheet.getColumn(5).width = 16
+	worksheet.getColumn(6).width = 12
+	worksheet.getColumn(7).width = 20
 
 	headerRow.eachCell((cell) => {
 		cell.border = {
@@ -35,7 +38,16 @@ export async function OrderUpload(data: OrderRetriveAllResponse['data'], res: Re
 	})
 
 	data.forEach((order, index) => {
-		const row = worksheet.addRow([index + 1, order.client.name, order.sum, order.seller.phone, '', order.debt, format(order.sellingDate, 'dd.MM.yyyy HH:mm')])
+		const row = worksheet.addRow([
+			index + 1,
+			order.client.name,
+			order.client.phone,
+			order.sum,
+			order.seller.phone,
+			'',
+			order.debt,
+			format(order.sellingDate, 'dd.MM.yyyy HH:mm'),
+		])
 
 		row.eachCell((cell) => {
 			cell.alignment = { vertical: 'middle', horizontal: 'center' }
