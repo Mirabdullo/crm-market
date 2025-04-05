3b07af69064ea81c5d59d86ee7a50a176c88fd91
woodlinepro <woodlineprodev@gmail.com>
2025-04-05 19:25:58 +0500

    ok

--- src/modules/admins/admin.service.ts
+++ src/modules/admins/admin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, ForbiddenException, NotFoundException } from '@nestjs/common'
 import { PrismaService } from '@prisma'
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 import {
 	AdminCreateRequest,
 	AdminDeleteRequest,
